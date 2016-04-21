ad62be8715f3de86862afe8a5964751141a0482b
Brian <brian@maxxpotential.com>
2016-04-20 23:10:23 -0400

    Removed a method

--- src/Node.java
+++ src/Node.java
@@ -26,10 +26,6 @@ public class Node {
         this.key = key;
     }
 
-    public Object setValue() {
-        return value;
-    }
-
     public void setValue(Object value) {
         this.value = value;
     }
